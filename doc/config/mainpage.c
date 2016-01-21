615dfaa3bb02af5138c13de30e6c6080642a0745
Alexis Lescouet <alexis.lescouet@telecom-sudparis.eu>
2016-01-21 02:44:56 +0100

    Add doxygen configuration file Add doxygen mainpage

---
+++ doc/config/mainpage.c
@@ -0,0 +1,32 @@
+/* Main Doxygen page */
+
+/** 
+ * @mainpage EPICS Project Reference
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the complete API reference for EPICS (EPICS is a Parallelism Image Computation Service)
+ *
+ * @subsection require Requirements
+ *
+ * You need to have the followings installed :
+ *
+ * 
+ *
+ * @subsection install Installation 
+ *
+ * To build the EPICS binary, you will need:
+ * * a C compiler
+ * * libfftw
+ * * libtiff
+ *
+ * To build the tests binary, you will need:
+ * * a C++ compiler
+ * * libfftw
+ * * libtiff
+ * * libgtest
+ * 
+ * @subsection run Just execute bin/epics or bin/runtests
+ *
+ */
+
