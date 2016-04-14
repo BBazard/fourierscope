32b62d026b91dcbadf39f8d837f257a6395ac1ee
Alexis Lescouet <alexis.lescouet@telecom-sudparis.eu>
2016-04-14 16:46:49 +0200

    Add empty main for compiling sake

---
+++ release/include/main.h
@@ -0,0 +1,12 @@
+/* Copyright [2016] <Alexis Lescouet, Benoit Bazard> */
+/**
+ *  @file
+ *
+ *  Main header
+ *
+ */
+
+#ifndef RELEASE_INCLUDE_MAIN_H_
+#define RELEASE_INCLUDE_MAIN_H_
+
+#endif /* RELEASE_INCLUDE_MAIN_H_ */
---
+++ release/src/main.c
@@ -0,0 +1,19 @@
+/* Copyright [2016] <Alexis Lescouet, Benoit Bazard> */
+/**
+ *  @file
+ *
+ *  This is the main file
+ *
+ */
+
+#include "include/main.h"
+
+/**
+ *  @brief Main function
+ *
+ *  @todo WRITE IT
+ *
+ */
+int main(int argc, char **argv) {
+  return 0;
+}
